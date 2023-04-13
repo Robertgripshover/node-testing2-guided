b5ead8181bc705562eaa11b418d427c00fc2b496
Robert Gripshover <robertgrips@gmail.com>
2023-04-13 16:13:01 -0400

    wrote a test for the getById function in the hobbits model

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Guided project for **Node Server Testing** Module.
 - [ ] clone this repository.
 - [ ] move inside the project folder.
 - [ ] type `npm i` to download dependencies.
+            `npm 
 - [ ] type `npm run migrate` to run migrations.
 - [ ] type `npm run seed` to seed the db.
 - [ ] type `npm run server` to start the API.
--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -19,6 +19,22 @@ describe('getAll', () => {
     test('resolves all the hobbits in the table', async () => {
         const result = await Hobbit.getAll()
         expect(result).toHaveLength(4)
+        expect(result[0]).toMatchObject({ name: 'sam' })
+        expect(result[1]).toMatchObject({ name: 'frodo' })
+        expect(result[2]).toMatchObject({ name: 'pippin' })
+        expect(result[3]).toMatchObject({ name: 'merry' })
+    })
+})
+
+describe('getById', () => {
+    test('resolves the hobbit by the given id', async () => {
+        let result = await Hobbit.getById(1)
         expect(result).toMatchObject({ name: 'sam' })
+        result = await Hobbit.getById(2)
+        expect(result).toMatchObject({ name: 'frodo' })
+        result = await Hobbit.getById(3)
+        expect(result).toMatchObject({ name: 'pippin' })
+        result = await Hobbit.getById(4)
+        expect(result).toMatchObject({ name: 'merry' })
     })
 })
