ce4fd5dec53c108a92b75d94cc2cce82ca8f0c60
Robert Gripshover <robertgrips@gmail.com>
2023-04-13 12:30:17 -0400

    made changes to the test files

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,3 +1,24 @@
+const db = require('../../data/dbConfig')
+
+const Hobbit = require('./hobbits-model')
+
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
+
+beforeEach(async () => {
+    await db.seed.run() //will run the seeds and truncate the tables
+})
+
 test('environment is testing', () => {
     expect(process.env.NODE_ENV).toBe('testing')
+})
+
+describe('getAll', () => {
+    test('resolves all the hobbits in the table', async () => {
+        const result = await Hobbit.getAll()
+        expect(result).toHaveLength(4)
+        expect(result).toMatchObject({ name: 'sam' })
+    })
 })
